e5dae3673e05cc2d4c4dd398b7a7de95dd1bf146
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-20 14:17:08 +0900

    CSS초기화, App.jsx초기화, 섹션구분

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <header className="app_header">Header Section</header>
+        <main className="app_main">
+          <section className="task_column">section1</section>
+          <section className="task_column">section2</section>
+          <section className="task_column">section3</section>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
