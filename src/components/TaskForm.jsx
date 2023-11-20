1de932eff451f64b5bed88150605f4cb5fd23a9a
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-20 16:29:11 +0900

    TaskForm(일정등록) 에 useState추가 입력값 콘솔 출력 완료

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import Tag from "./Tag";
 import "./TaskForm.css";
 
 const TaskForm = () => {
+  const [taskData, setTaskData] = useState({ task: "", status: "todo" }); // 여러개 입력 객체로 관리
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
+          onChange={handleChange}
+          value={taskData.task}
+          name="task"
           className="task_input"
           placeholder="일정을 입력해 주세요!"
         />
@@ -19,10 +36,15 @@ const TaskForm = () => {
             <Tag tagName="REACT" />
           </div>
           <div>
-            <select className="task_status">
+            <select
+              className="task_status"
+              name="status"
+              value={taskData.status}
+              onChange={handleChange}
+            >
               <option value="todo">할일</option>
-              <option value="todo">진행중</option>
-              <option value="todo">완료</option>
+              <option value="doing">진행중</option>
+              <option value="done">완료</option>
             </select>
             <button type="submit" className="task_submit">
               + 추가
