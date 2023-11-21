3e09e821f6a1f1e08a887b41cab6907e8cc54640
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-21 09:33:26 +0900

    태그 선택 시 taskData에 추가 및 버튼 배경 색상 변경

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,7 +1,22 @@
 import "./Tag.css";
 
-const Tag = ({ tagName }) => {
-  return <button className="tag">{tagName}</button>;
+const Tag = ({ tagName, selectTag, selected }) => {
+  const tagStyle = {
+    HTML: { backgroundColor: "#fda821" },
+    CSS: { backgroundColor: "#15d4c8" },
+    JavaScript: { backgroundColor: "#ffd12c" },
+    REACT: { backgroundColor: "#4cdafc" },
+    default: { backgroundColor: "#f9f9f9" },
+  };
+  return (
+    <button
+      className="tag"
+      onClick={() => selectTag(tagName)}
+      style={selected ? tagStyle[tagName] : tagStyle.default}
+    >
+      {tagName}
+    </button>
+  );
 };
 
 export default Tag;
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -3,7 +3,11 @@ import Tag from "./Tag";
 import "./TaskForm.css";
 
 const TaskForm = () => {
-  const [taskData, setTaskData] = useState({ task: "", status: "todo" }); // 여러개 입력 객체로 관리
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+    tags: [],
+  }); // 여러개 입력 객체로 관리
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -15,6 +19,25 @@ const TaskForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(taskData);
+  };
+
+  //선택한 태그를 tags에 추가한다( 있으면 삭제 없으면 추가)
+  const selectTag = (tag) => {
+    if (taskData.tags.some((item) => item === tag)) {
+      const filterTags = taskData.tags.filter((item) => item !== tag);
+      setTaskData((prev) => {
+        return { ...prev, tags: filterTags };
+      });
+    } else {
+      setTaskData((prev) => {
+        return { ...prev, tags: [...prev.tags, tag] };
+      });
+    }
+  };
+  console.log(taskData.tags); //태그 선택 확인
+  //선택된 태그 아이템 중 tag가 있으면 true없으면 false
+  const checkTag = (tag) => {
+    return taskData.tags.some((item) => item === tag);
   };
   return (
     <header className="app_header">
@@ -30,10 +53,26 @@ const TaskForm = () => {
 
         <div className="task_form_bottom_line">
           <div>
-            <Tag tagName="HTML" />
-            <Tag tagName="CSS" />
-            <Tag tagName="JavaScript" />
-            <Tag tagName="REACT" />
+            <Tag
+              tagName="HTML"
+              selectTag={selectTag}
+              selected={checkTag("HTML")}
+            />
+            <Tag
+              tagName="CSS"
+              selectTag={selectTag}
+              selected={checkTag("CSS")}
+            />
+            <Tag
+              tagName="JavaScript"
+              selectTag={selectTag}
+              selected={checkTag("JavaScript")}
+            />
+            <Tag
+              tagName="REACT"
+              selectTag={selectTag}
+              selected={checkTag("REACT")}
+            />
           </div>
           <div>
             <select
